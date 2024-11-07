45ce29fc5a067426f21ddb8869fd0d1a848c8855
James Floyd <98607522+FloatySMG@users.noreply.github.com>
2024-11-07 17:30:52 -0500

    kyra this was so easy i just needed trial and error (cutscene 1 works)

--- PROJECT FINAL WORK WITH THIS!!/Cyber Haven/MovieRGF.java
+++ PROJECT FINAL WORK WITH THIS!!/Cyber Haven/MovieRGF.java
@@ -1,27 +1,27 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Movie here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MovieRGF extends Actor
 {   
     GifImage myGif = new GifImage("MovieB.gif");
-    
-    // Define the width and height you want for scaling
     private int scaleWidth = 1200;  // Set to desired width
-    private int scaleHeight = 600; // Set to desired height
+    private int scaleHeight = 600;  // Set to desired height
 
-    /**
-     * Act - do whatever the Movie wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int frameCount = 0;     // Frame counter
+
     public void act()
     {
+       // Increment the frame counter each time act is called
+       frameCount++;
+
+       // Display the current frame of the GIF
        GreenfootImage currentImage = myGif.getCurrentImage();
-       currentImage.scale(scaleWidth, scaleHeight);  // Scale the image
-       setImage(currentImage);  // Set the scaled image
+       currentImage.scale(scaleWidth, scaleHeight);
+       setImage(currentImage);
+
+       // Check if the frame count has reached 114
+       if (frameCount >= 600) {
+           // Transition to TutorialStageA
+           Greenfoot.setWorld(new TutorialStageA());
+       }
     }
 }
