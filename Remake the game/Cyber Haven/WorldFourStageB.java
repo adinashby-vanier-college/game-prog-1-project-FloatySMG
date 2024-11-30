56840b8de1cd3b2a15b00f8adfa32f9ce4a996b0
James Floyd <jamesaydenfloyd1@gmail.com>
2024-11-29 23:58:02 -0500

    see

--- Remake the game/Cyber Haven/WorldFourStageB.java
+++ Remake the game/Cyber Haven/WorldFourStageB.java
@@ -15,5 +15,25 @@ public class WorldFourStageB extends WorldFour
      */
     public WorldFourStageB()
     {
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+
+        LevelFourStepC levelFourStepC = new LevelFourStepC();
+        addObject(levelFourStepC,50,441);
+        LevelFourStepC levelFourStepC2 = new LevelFourStepC();
+        addObject(levelFourStepC2,152,441);
+        CyrusPlayer cyrusPlayer = new CyrusPlayer();
+        addObject(cyrusPlayer,22,381);
+        Cannon cannon = new Cannon();
+        addObject(cannon,79,382);
+        BounceMedium bounceMedium = new BounceMedium();
+        addObject(bounceMedium,287,428);
     }
 }
